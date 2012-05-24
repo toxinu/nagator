99fd8420442e3f986ef177ca22f0d95f7011a00c
toxinu <toxinu@gmail.com>
2012-05-24 22:14:01 +0200

    Change bin to scripts

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import sys
 from setuptools import setup
 
 setup(
-    name = 'nagator',
-    version = '0.0.2',
-    author = "Socketubs",
-    author_email = "geoffrey@lehee.name",
-    description = "Nagios configuration viewer",
-    license = "No Licence",
-    keywords = "nagios cli parser viewer",
-    url = "https://github.com/Socketubs/Nagator",
+	name = 'nagator',
+	version = '0.0.2',
+	author = "Socketubs",
+	author_email = "geoffrey@lehee.name",
+	description = "Nagios configuration viewer",
+	license = "No Licence",
+	keywords = "nagios cli parser viewer",
+	url = "https://github.com/Socketubs/Nagator",
 	data_files=[('/etc', ['conf/nagator.cfg'])],
-    packages = ['nagator'],
-    scripts = ['bin/nagator'],
-    install_requires=['pynag', 'clint']
+	packages = ['nagator'],
+	scripts = ['scripts/nagator'],
+	install_requires=['pynag', 'clint']
 )
